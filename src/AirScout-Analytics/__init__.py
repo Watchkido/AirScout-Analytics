f659e02920df9041b00e1b0624b0d7432338a652
Frank Albrecht <44406884+Watchkido@users.noreply.github.com>
2025-07-19 10:44:57 +0200

    Initial commit

---
+++ src/AirScout-Analytics/__init__.py
@@ -0,0 +1,7 @@
+"""
+AirScout-Analytics  watchkido
+visualization.py
+Funktionen zur Visualisierung von Daten.
+Hier werden Plots und Diagramme erstellt.
+"""
+
---
+++ src/AirScout-Analytics/reporting.py
@@ -0,0 +1,13 @@
+"""
+AirScout-Analytics  watchkido
+reporting.py
+Funktionen zur Berichterstellung.
+Hier werden Berichte oder Präsentationen aus Analyseergebnissen erzeugt.
+"""
+from config import CONFIG
+def create_report(results):
+    print('Report:', results)
+        
+if __name__ == "__main__":
+    # Code hier drunter wird nur ausgeführt wenn das Skript direkt aufgerufen wird
+    pass
